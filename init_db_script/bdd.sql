02e406a5cfcd1c5c44e61e7f89b6aa5d36fe50ac
eric.girard <delasalle.sio.girard.e@gmail.com>
2024-12-06 11:40:12 +0100

    ajout script bdd + ajout dans le compose

---
+++ init_db_script/bdd.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS mspr_database;
+CREATE DATABASE mspr_database;
+
+USE mspr_database;
+
+DROP USER IF EXISTS mspr_user;
+CREATE USER 'mspr_user'@'%' IDENTIFIED BY 'mspr_user';
+GRANT ALL PRIVILEGES ON mspr_database.* TO 'mspr_user'@'%';
+
+
+
+CREATE TABLE Localization(
+    id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(30) UNIQUE NOT NULL,
+    continent varchar(20) NOT NULL
+);
+
+CREATE TABLE ReportCase(
+    id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    totalConfirmed INT(10) NOT NULL,
+    totalRecoveries INT(10) NOT NULL,
+    totalDeath INT(10) NOT NULL,
+    totalActive INT(10) NOT NULL,
+    localizationId INT(5) NOT NULL,
+    date DATE,
+    FOREIGN KEY (localizationId) REFERENCES Localization(id)
+);
