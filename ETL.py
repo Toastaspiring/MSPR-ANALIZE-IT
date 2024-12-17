02757ee82317f0d07b4bbc97bc4ac38b8329cf3b
Louis Marec <113687926+Toastaspiring@users.noreply.github.com>
2024-12-17 16:01:48 +0100

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -3,15 +3,15 @@ import time
 import pandas as pd
 import mysql.connector
 
-start_time=time.time()
-# Load datasets
-coronavirus_data = pd.read_csv('files/worldometer_coronavirus_daily_data.csv')
-countries_continents = pd.read_csv('files/countries_and_continents.csv')
-monkeypox_data = pd.read_csv('files/owid-monkeypox-data.csv')
+start_time = time.time()
+
+# Load datasets and replace NaN with 0
+coronavirus_data = pd.read_csv('files/worldometer_coronavirus_daily_data.csv').fillna(0)
+countries_continents = pd.read_csv('files/countries_and_continents.csv').fillna(0)
+monkeypox_data = pd.read_csv('files/owid-monkeypox-data.csv').fillna(0)
 monkeypox_data = monkeypox_data[~monkeypox_data['iso_code'].str.contains("OWID", na=False)]
 
-# ajustement donnees
-
+# Adjustment function for country names
 def rename_country(country_name):
     if 'Bosnia and Herzegovina' in country_name:  return 'Bosnia And Herzegovina'
     elif 'Vietnam' in country_name: return 'Viet Nam'
@@ -20,19 +20,22 @@ def rename_country(country_name):
     elif 'United Kingdom' in country_name: return 'UK'
     elif 'United States' in country_name: return 'USA'
     else:
-        return country_name  # Sinon, conserver la valeur d'origine
+        return country_name  # Otherwise, keep the original value
 
 monkeypox_data['location'] = monkeypox_data['location'].apply(rename_country)
 
+# Replace NaN in monkeypox_data after renaming
+monkeypox_data = monkeypox_data.fillna(0)
+
 # Create Disease Table
-diseases = pd.DataFrame({'id': [1, 2], 'name': ['Coronavirus', 'Monkeypox']})
+diseases = pd.DataFrame({'id': [1, 2], 'name': ['Coronavirus', 'Monkeypox']}).fillna(0)
 
 # Create Localization Table
 countries_continents['id'] = range(1, len(countries_continents) + 1)
-localization = countries_continents.rename(columns={'country': 'country', 'continent': 'continent'})
+localization = countries_continents.rename(columns={'country': 'country', 'continent': 'continent'}).fillna(0)
 
 # Merge Coronavirus Data with Localization
-coronavirus_data = coronavirus_data.merge(countries_continents, how='left', left_on='country', right_on='country')
+coronavirus_data = coronavirus_data.merge(countries_continents, how='left', left_on='country', right_on='country').fillna(0)
 
 # Prepare ReportCase Data for Coronavirus
 coronavirus_data['diseaseId'] = 1  # Coronavirus ID
@@ -42,7 +45,7 @@ coronavirus_report_cases = coronavirus_data.rename(columns={
     'active_cases': 'totalActive',
     'date': 'date',
     'id': 'localizationId'
-})[['totalconfirmed', 'totalDeath', 'totalActive', 'localizationId', 'date', 'diseaseId']]
+})[['totalconfirmed', 'totalDeath', 'totalActive', 'localizationId', 'date', 'diseaseId']].fillna(0)
 
 # Prepare ReportCase Data for Monkeypox
 monkeypox_data = monkeypox_data.rename(columns={
@@ -50,15 +53,15 @@ monkeypox_data = monkeypox_data.rename(columns={
     'total_deaths': 'totalDeath',
     'location': 'country',
     'date': 'date'
-})
-monkeypox_data = monkeypox_data.merge(countries_continents, how='left', on='country')
+}).fillna(0)
+monkeypox_data = monkeypox_data.merge(countries_continents, how='left', on='country').fillna(0)
 monkeypox_data['diseaseId'] = 2  # Monkeypox ID
 monkeypox_report_cases = monkeypox_data.rename(columns={
     'id': 'localizationId'
-})[['totalconfirmed', 'totalDeath', 'localizationId', 'date', 'diseaseId']]
+})[['totalconfirmed', 'totalDeath', 'localizationId', 'date', 'diseaseId']].fillna(0)
 
 # Combine ReportCase Data
-report_cases = pd.concat([coronavirus_report_cases, monkeypox_report_cases], ignore_index=True)
+report_cases = pd.concat([coronavirus_report_cases, monkeypox_report_cases], ignore_index=True).fillna(0)
 
 # MySQL Database connection parameters
 db_config = {
@@ -86,7 +89,7 @@ for index, row in localization.iterrows():
 # Insert into ReportCase Table
 for index, row in report_cases.iterrows():
     os.system('clear')
-    print("nb ligne traité : "+ str(index))
+    print("nb ligne traité : " + str(index))
     cursor.execute("""
         INSERT INTO ReportCase (totalconfirmed, totalDeath, totalActive, localizationId, date, diseaseId) 
         VALUES (%s, %s, %s, %s, %s, %s)""",
