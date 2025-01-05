2e52f6bc88534e8a2a4e9eeece4ce238ccc4e84c
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2025-01-05 13:48:11 +0100

    Doc for route /get/sorted

--- api/src/reportCases/reportCases.controller.ts
+++ api/src/reportCases/reportCases.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Query } from '@nestjs/common';
 import { ReportCaseService } from './reportCases.service';
+import { ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('case')
 export class ReportCaseController {
@@ -8,9 +9,15 @@ export class ReportCaseController {
     ){}
 
     @Get('/get/sorted')
-    async getSortedReportCases(@Body() body){
-        const count = body.count ? body.count : 1000
-        const sort = body.sort ? body.sort : ""
+    @ApiQuery({ name: 'sort', required: false, type: String, description: 'The filter to retrieve specific data' })
+    @ApiQuery({ name: 'count', required: false, type: Number, description: 'The number of cases to retrieve', default: 1000 })
+    @ApiResponse({ status: 200, description: 'The list of sorted report cases has been retrieved successfully.' })
+    @ApiResponse({ status: 400, description: 'Invalid query parameters.' })
+    async getSortedReportCases(@Query('sort') sort: string = "",@Query('count') count: number = 1000)
+    {
+        if (count <= 0) {
+            throw new BadRequestException('Count must be a positive number.');
+        }
 
         const reportCases = await this.caseService.getSortedReportCases(sort,count);
         return reportCases
