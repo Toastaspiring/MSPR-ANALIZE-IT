132366601c4fbe778708c24bee65e8471ec8a736
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-01-06 10:41:52 +0100

    modif des port pour docker

--- analyze_it/vite.config.ts
+++ analyze_it/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Autorise aussi localhost
+    port: 5173,  // Optionnel, mais utile pour fixer le port
+  },
 })
