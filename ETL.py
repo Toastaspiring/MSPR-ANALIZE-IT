7e8e6ee3ad63f20b872dd3f78e048ce356af1651
Toastaspiring <lmarec.wows@gmail.com>
2025-04-07 16:36:55 +0200

    nodata error handling

--- ETL.py
+++ ETL.py
@@ -34,7 +34,7 @@ def insert_archive():
         df = pd.read_csv(file, dtype=str)
         df = df.applymap(lambda x: 0 if isinstance(x, str) and x.strip().lower() in ['no data', 'n/a'] else x)
         df.fillna(0, inplace=True)
-        df.replace(to_replace=r"^\s*(no[\s_-]?data|n/?a)\s*$", value=0, regex=True, inplace=True, case=False)
+        df.replace(to_replace=r"(?i)^\s*(no[\s_-]?data|n/?a)\s*$", value=0, regex=True, inplace=True)
         df.fillna(0, inplace=True)
 
         if table == "millions_population_country":
