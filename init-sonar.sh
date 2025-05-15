3853885d0ab11d89d7479239c1457003135f7df3
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-05-15 14:43:12 +0200

    modif pour sonar

--- init-sonar.sh
+++ init-sonar.sh
@@ -2,19 +2,19 @@
 
 # Attendre que SonarQube soit prêt
 echo "Waiting for SonarQube to be available..."
-until curl -s -u admin:admin http://sonarqube:9000/api/system/health | grep -q '"status":"GREEN"'; do
+until curl -s -u admin:admin http://host.docker.internal:9000/api/system/health | grep -q '"status":"GREEN"'; do
   sleep 5
 done
 
 echo "SonarQube is up. Configuring..."
 
 # Changer le mot de passe admin
-curl -s -u admin:admin -X POST "http://sonarqube:9000/api/users/change_password?login=admin&previousPassword=admin&password=newpassword"
+curl -s -u admin:admin -X POST "http://host.docker.internal:9000/api/users/change_password?login=admin&previousPassword=admin&password=newpassword"
 
 # Créer un token pour l’analyse
-curl -s -u admin:newpassword -X POST "http://sonarqube:9000/api/user_tokens/generate?name=ci-token"
+curl -s -u admin:newpassword -X POST "http://host.docker.internal:9000/api/user_tokens/generate?name=ci-token"
 
 # Créer un projet (ex: `my-github-repo`)
-curl -s -u admin:newpassword -X POST "http://sonarqube:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
+curl -s -u admin:newpassword -X POST "http://host.docker.internal:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
 
 echo "Configuration done."
