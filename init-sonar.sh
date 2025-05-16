b6bd975d060fede1f8cf4d74aaa21ba5f7c8dea7
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-05-16 09:40:44 +0200

    modif init sonar

--- init-sonar.sh
+++ init-sonar.sh
@@ -9,12 +9,12 @@ done
 echo "SonarQube is up. Configuring..."
 
 # Changer le mot de passe admin
-curl -s -u admin:admin -X POST "http://localhost:9000/api/users/change_password?login=admin&previousPassword=admin&password=P75:5,0_{I7}lh&"
+curl -s -u admin:admin -X POST "http://localhost:9000/api/users/change_password?login=admin&previousPassword=admin&password=P75:5,0_{I7}lh"
 
 # Créer un token pour l’analyse
-curl -s -u admin:P75:5,0_{I7}lh& -X POST "http://localhost:9000/api/user_tokens/generate?name=ci-token"
+curl -s -u admin:P75:5,0_{I7}lh -X POST "http://localhost:9000/api/user_tokens/generate?name=ci-token"
 
 # Créer un projet (ex: `my-github-repo`)
-curl -s -u admin:P75:5,0_{I7}lh& -X POST "http://localhost:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
+curl -s -u admin:P75:5,0_{I7}lh -X POST "http://localhost:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
 
 echo "Configuration done."
