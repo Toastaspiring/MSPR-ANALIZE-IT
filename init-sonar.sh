d201aa03a931fab35a48ad650cfc7ef33a6607ec
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-05-16 14:37:12 +0200

    ajoutement phpmyadmin et ajout de plugins sonar

--- init-sonar.sh
+++ init-sonar.sh
@@ -2,19 +2,19 @@
 
 # Attendre que SonarQube soit prêt
 echo "Waiting for SonarQube to be available..."
-until curl -s -u admin:admin http://localhost:9000/api/system/health | grep -q '"status":"GREEN"'; do
+until curl -s -u admin:admin http://localhost:9000/api/system/health | grep -q '"health":"GREEN"'; do
   sleep 5
 done
 
 echo "SonarQube is up. Configuring..."
 
 # Changer le mot de passe admin
-curl -s -u admin:admin -X POST "http://localhost:9000/api/users/change_password?login=admin&previousPassword=admin&password=P75:5,0_{I7}lh"
+curl -s -u admin:admin -X POST "http://localhost:9000/api/users/change_password?login=admin&previousPassword=admin&password=P75:5,0_I7lh"
 
 # Créer un token pour l’analyse
-curl -s -u admin:P75:5,0_{I7}lh -X POST "http://localhost:9000/api/user_tokens/generate?name=ci-token"
+curl -s -u admin:P75:5,0_I7lh -X POST "http://localhost:9000/api/user_tokens/generate?name=ci-token"
 
 # Créer un projet (ex: `my-github-repo`)
-curl -s -u admin:P75:5,0_{I7}lh -X POST "http://localhost:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
+curl -s -u admin:P75:5,0_I7lh -X POST "http://localhost:9000/api/projects/create?name=my-github-repo&project=my-github-repo"
 
 echo "Configuration done."
